400315e7d71b9e5c9970bc9863dcde691e8aa5a5
keygjones <karl.gunnar.jonsson@gmail.com>
2016-02-05 14:12:23 +0100

    ready to push

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static('client'));
 
-app.listen(port, function () {
-    console.log('Server running on http://localhost:' + port + '/');
+app.listen(port, function callback () {
+    console.log('Running our app at http://localhost:3000')
 });
